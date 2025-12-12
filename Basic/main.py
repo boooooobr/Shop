6ad64c60d6646e1c929ef8bca592a41a8d74bd2b
boooooobr <lexabot213@gmail.com>
2025-12-12 12:13:34 +0500

    Мелкте дороботки

--- Basic/main.py
+++ Basic/main.py
@@ -3,7 +3,7 @@ from config import *
 from telebot import TeleBot
 from telebot import types
 
-bot = TeleBot('TOKEN')
+bot = TeleBot('7689614483:AAG5n7-NSiWPNh3XMYKIwF1aCXqKz_ljPho')
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
